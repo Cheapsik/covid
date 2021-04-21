0400b5d1cb0a1d5a86d4a3da4d5c1f83f364900f
Łukasz Fijał <cheapsik@gmail.com>
2021-04-21 19:20:29 +0200

    feat: getting data from covid api

--- src/Covid.js
+++ src/Covid.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Card, CountryPicker, Footer } from './components'
 import styles from './Covid.module.scss';
+import { fetchData } from './api';
 
 
 class Covid extends React.Component {
   state = { 
-    
+      data: {},
    }
+
+  async componentDidMount() {
+     const covidDate = await fetchData()
+    
+     this.setState({
+       data: covidDate,
+     })
+   }
+
   render() { 
     return ( 
       <div className={styles.app}>
