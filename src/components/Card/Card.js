03f39d0a97bb0a1d660e32086e1f1c66812b86da
Łukasz Fijał <cheapsik@gmail.com>
2021-04-21 20:23:31 +0200

    feat(Card): creating structure and preliminary styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,66 @@
+import {
+  Card as Cardd,
+  CardContent,
+  Typography,
+  Grid,
+} from "@material-ui/core";
+import CountUp from "react-countup";
+import { RiVirusFill as Virus } from 'react-icons/ri';
+import { GiHealthNormal as Hearth } from 'react-icons/gi';
+import { FaSkull as Skull } from 'react-icons/fa';
 
-const Card = () => {
-    return ( <p>
-        Siemka
-    </p> );
-}
- 
+const Card = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loading...";
+  }
+
+  return (
+    <div className="container">
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Cardd}>
+          <CardContent>
+            <Typography  align="center">
+              <Virus /> Infected:
+            </Typography>
+
+            <Typography color="textSecondary" align="center">
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2}
+                separator=","
+              />
+            </Typography>
+
+            <Typography align="center">
+              <Hearth />Recovered:
+            </Typography>
+            <Typography color="textSecondary" align="center">
+              <CountUp
+                start={0}
+                end={recovered.value}
+                duration={2}
+                separator=","
+              />
+            </Typography>
+            <Typography align="center">
+              <Skull />Deaths:
+            </Typography>
+            <Typography color="textSecondary" align="center">
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={2}
+                separator=","
+              />
+            </Typography>
+            <br />
+            <Typography variant="body2" style={{ fontSize: 12 }}>{lastUpdate}</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
 export default Card;
