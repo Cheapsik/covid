63803ea9c1fd67cec4ee42ab8cd00b78f1322c6c
Łukasz Fijał <cheapsik@gmail.com>
2021-04-22 17:36:25 +0200

    feat(Card): Added preliminarily structure and styles for it

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,64 +1,56 @@
-import {
-  Card as Cardd,
-  CardContent,
-  Typography,
-  Grid,
-} from "@material-ui/core";
 import CountUp from "react-countup";
-import { RiVirusFill as Virus } from 'react-icons/ri';
-import { GiHealthNormal as Hearth } from 'react-icons/gi';
-import { FaSkull as Skull } from 'react-icons/fa';
+import React, { useState, useEffect } from "react";
+import { RiVirusFill as Virus } from "react-icons/ri";
+import { GiHealthNormal as Hearth } from "react-icons/gi";
+import { FaSkull as Skull } from "react-icons/fa";
+import { fetchCountries} from "../../api";
+import styles from "./Card.module.scss";
 
-const Card = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
-  if (!confirmed) {
-    return "Loading...";
-  }
+const Card = ({ data: { confirmed, recovered, deaths } }) => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries());
+    };
+
+    fetchAPI();
+  }, [setFetchedCountries]);
 
   return (
-    <div className="container">
-      <Grid container spacing={3} justify="center">
-        <Grid item component={Cardd}>
-          <CardContent>
-            <Typography  align="center">
-              <Virus /> Infected:
-            </Typography>
-
-            <Typography color="textSecondary" align="center">
-              <CountUp
-                start={0}
-                end={confirmed.value}
-                duration={2}
-                separator=","
-              />
-            </Typography>
-
-            <Typography align="center">
-              <Hearth />Recovered:
-            </Typography>
-            <Typography color="textSecondary" align="center">
-              <CountUp
-                start={0}
-                end={recovered.value}
-                duration={2}
-                separator=","
-              />
-            </Typography>
-            <Typography align="center">
-              <Skull />Deaths:
-            </Typography>
-            <Typography color="textSecondary" align="center">
-              <CountUp
-                start={0}
-                end={deaths.value}
-                duration={2}
-                separator=","
-              />
-            </Typography>
-            <br />
-            <Typography variant="body2" style={{ fontSize: 12 }}>{lastUpdate}</Typography>
-          </CardContent>
-        </Grid>
-      </Grid>
+    <div className={styles.container}>
+      {fetchedCountries.map((country, id) => (
+        <div className={styles.card} key={id}>
+          <p>{country}</p>
+          <p>
+            <Virus /> Infected:
+          </p>
+          <span>
+            <CountUp
+              start={0}
+              end={confirmed.value}
+              duration={2}
+              separator=","
+            />
+          </span>
+          <p>
+            <Hearth /> Recovered:
+          </p>
+          <span>
+            <CountUp
+              start={0}
+              end={recovered.value}
+              duration={2}
+              separator=","
+            />
+          </span>
+          <p>
+            <Skull /> Deaths:
+          </p>
+          <span>
+            <CountUp start={0} end={deaths.value} duration={2} separator="," />
+          </span>
+        </div>
+      ))}
     </div>
   );
 };
