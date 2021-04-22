b6a756be2369651e094b7a3ff049c294833052e5
Łukasz Fijał <cheapsik@gmail.com>
2021-04-22 17:34:13 +0200

    feat(Footer): adding footer and styles

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+    return ( 
+        <p className={styles.footer}>Remember to keep social distance, wear a mask and wash your hands.</p>
+     );
+}
+ 
+export default Footer;
