29102bc3d30c37feb64287563185bef9d1b7a4d8
Łukasz Fijał <cheapsik@gmail.com>
2021-04-23 19:22:11 +0200

    feat: Added country picker - 'select' and styles for him

--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -1,7 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import {NativeSelect, FormControl} from '@material-ui/core';
+import styles from './CountryPicker.module.scss';
+import { fetchCountries } from '../../api';
 
-const CountryPicker = () => {
-    return ( 
-        <p>Tak</p>
+const CountryPicker = ({handleCountryChange}) => {
+
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFetchedCountries(await fetchCountries());
+        }
+
+        fetchAPI();
+    }, [setFetchedCountries]);
+    
+    return (
+        <div className={styles.container}>
+        <FormControl>
+            <NativeSelect className={styles.select} defaultValue="" onChange={(e) => {handleCountryChange(e.target.value)}}>
+                <option className={styles.mainOption}  value="global">Choose country</option>
+                {fetchedCountries.map((country,id) => <option key={id} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
+        </div>
      );
 }
  
